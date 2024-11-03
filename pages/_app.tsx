f830f29372cc535a9fa761426756ba795df2dfde
Ihor Antoniuk <igorantonyuk@gmail.com>
2024-11-03 22:29:31 +0100

    Preventing hydrating problem while refreshing html page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/style.css'; // Import the global CSS file
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Ensures content only renders after mount on client
+
   return <Component {...pageProps} />;
 }
 
